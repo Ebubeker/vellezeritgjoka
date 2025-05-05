3e519aab6ed9cdbe5e3583f63a3117f6790d8ac8
Ebubeker Rexha <60944813+Ebubeker@users.noreply.github.com>
2025-05-05 21:53:56 +0200

    wip: created about components

--- app/about/page.js
+++ app/about/page.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import Hero from '@/components/about/Hero'
+import WhoAreWe from '@/components/about/WhoAreWe'
+import CompanyInfo from '@/components/about/CompanyInfo'
+import Counter from '@/components/about/Counter'
+import WhyUs from '@/components/about/WhyUs'
 
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
+    <div>
+      <Hero />
+      <WhoAreWe />
+      <CompanyInfo />
+      <CompanyInfo />
+      <Counter />
+      <WhyUs />
+    </div>
   )
 }
 
