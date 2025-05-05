fd1aee1eb835f807ed6d7fa5d476168bf26651c6
Ebubeker Rexha <60944813+Ebubeker@users.noreply.github.com>
2025-05-05 20:27:12 +0200

    feat: added tailwind

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="text-3xl font-bold underline mt-20">
       Home page
     </div>
   );
